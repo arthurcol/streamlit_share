6fda696428ad71f317542dac9751a955b70f9b22
Arthur Collard <arthur.collard@hotmail.fr>
2022-10-12 12:07:21 +0200

    initial commit

---
+++ app/pages/page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("This would be page 1")
+
+if st.button("Click here and see what happens"):
+    st.snow()
---
+++ app/pages/page2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("This would be page 2")
+
+if st.button("Click here and see what happens"):
+    st.balloons()
